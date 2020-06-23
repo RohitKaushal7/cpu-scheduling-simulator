7314662e8d5ba7ccddc0f2482ce51f12e092d7e6
RohitKaushal7 <rohitkk252550@gmail.com>
2020-06-23 10:26:15 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name='cpu-scheduling-simulator', version='1.0', packages=find_packages())
+setup(name='cpu-scheduling-simulator',
+      version='1.0',
+      packages=find_packages(),
+      install_requires=['autopep8', 'cycler', 'kiwisolver', 'matplotlib',
+                        'numpy', 'pycodestyle', 'pyparsing', 'python-dateutil', 'six', 'toml']
+      )
