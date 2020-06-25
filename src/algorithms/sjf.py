2236084071bc035859ea23c2da8e455d02e61aa8
vishalshrivastava16 <vishushrivastava16012000@gmail.com>
2020-06-25 16:48:08 +0530

    sjf and sjf.preemptive

---
+++ src/algorithms/sjf.py
@@ -0,0 +1,76 @@
+# when imported as module.
+from src.utils.tmp import processes
+import src.utils.table as table
+
+
+def run(processes):
+    # expects array of processes as arguments -> run the scheduling -> returns a dictionary/object of the result.
+
+    # initialize
+    total_waiting_time = 0
+    total_turnaround_time = 0
+    total_response_time = 0
+    total_return_time = 0
+
+    # sort by arrival_time
+    proc = sorted(processes, key=lambda proc: proc.arrival_time)
+
+    # setting initial values
+    proc[0].return_time = proc[0].burst_time
+    proc[0].turnaround_time = proc[0].return_time - proc[0].arrival_time
+    proc[0].response_time = 0
+    proc[0].waiting_time = 0
+
+    #simulating the process
+    for i in range(1,len(proc)):
+
+        tem = proc[i-1].return_time
+        low = proc[i].burst_time
+        val = 0
+        for j in  range(i,len(proc)):
+            if tem >= proc[j].arrival_time and low >= proc[j].burst_time:
+                low = proc[j].burst_time
+                val = j
+        proc[val].return_time = tem + proc[val].burst_time
+        proc[val].turnaround_time = proc[val].return_time - proc[val].arrival_time
+        proc[val].waiting_time = proc[val].turnaround_time - proc[val].burst_time
+        proc[i] , proc[val] = proc[val] , proc[i]
+    
+   
+    
+  
+    # update total
+    total_response_time = proc[0].response_time
+    total_waiting_time = proc[0].waiting_time
+    total_turnaround_time = proc[0].turnaround_time
+    total_return_time = proc[0].burst_time
+
+    # calculate for next processes
+    for i in range(1, len(proc)):
+
+        # update total
+        total_response_time += proc[i].response_time
+        total_waiting_time += proc[i].waiting_time
+        total_turnaround_time += proc[i].turnaround_time
+        total_return_time += proc[i].burst_time
+
+    return {
+        'avg_waiting_time': total_waiting_time/len(proc),
+        'avg_response_time': total_response_time/len(proc),
+        'avg_turnaround_time': total_turnaround_time/len(proc),
+        'processes': proc
+    }
+
+
+# If this file is executed directly -> run temporary test-cases
+def main():
+    result = run(processes)
+    print("Avg Waiting Time: {}".format(result['avg_waiting_time']))
+    print("Avg Turnaround Time: {}".format(result['avg_turnaround_time']))
+    print("Avg Response Time: {}".format(result['avg_response_time']))
+    table.plot(result['processes'])
+
+
+if __name__ == '__main__':
+    main()
+
--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
 import src.algorithms.fcfs as fcfs
+import src.algorithms.sjf as sjf
+import src.algorithms.sjf_preemptive as sjf_preemptive
 from src.utils.tmp import processes  # tmp processes
 
 # Actual App logic goes in this main function. Like all the options to choose from algorithms input outputs etc.
@@ -7,3 +9,5 @@ from src.utils.tmp import processes  # tmp processes
 def main():
     # fcfs.run(processes)
     fcfs.main()
+    sjf.main()
+    sjf_preemptive.main()
