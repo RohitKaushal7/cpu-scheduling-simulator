b9ceb182240fa0510eff35dc6de24858e9a8345e
vishalshrivastava16 <vishushrivastava16012000@gmail.com>
2020-06-30 12:51:53 +0530

    corrected sjf_preemptive

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import src.algorithms.fcfs as fcfs
 import src.algorithms.sjf as sjf
 import src.algorithms.sjf_preemptive as sjf_preemptive
+import src.algorithms.priority_non_preemptive as priority_non_preemptive
 from src.utils.tmp import processes  # tmp processes
 
 # Actual App logic goes in this main function. Like all the options to choose from algorithms input outputs etc.
@@ -11,3 +12,4 @@ def main():
     fcfs.main()
     sjf.main()
     sjf_preemptive.main()
+    priority_non_preemptive.main()
