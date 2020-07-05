af469f11fc23a3280276780ef85f2fa70eacdca0
RohitKaushal7 <rohitkk252550@gmail.com>
2020-07-05 12:12:51 +0530

    removed source code from documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,9 +42,9 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
 ]
+    # 'sphinx.ext.viewcode',
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
