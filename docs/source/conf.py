d6e7c3c3378bb667105ac9ccc78a4d66d1362324
RohitKaushal7 <rohitkk252550@gmail.com>
2020-07-22 11:19:06 +0530

    view code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,8 +43,8 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
     'sphinx.ext.githubpages',
+    'sphinx.ext.viewcode',
 ]
-    # 'sphinx.ext.viewcode',
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
